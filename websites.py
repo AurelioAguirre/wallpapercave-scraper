164b434e9c38354a03d6cfaa6d7d8436837990b7
AurelioAguirre <aureliofaguirre@gmail.com>
2022-08-11 23:38:59 +0200

    Fixed bugs, made it so files don't overwrite.

--- websites.py
+++ websites.py
@@ -3,8 +3,10 @@ import shutil, os
 from bs4 import BeautifulSoup as bs
 
 class Wallpapercave:
+	headers = {"User-Agent":"MadeUpBrowser 1.0"}
 	default = "wallpapercave.com"
 	http = "https://"
+
 	def __init__ (self, url):
 		self.is_default = False
 		self.url = url
@@ -19,8 +21,7 @@ class Wallpapercave:
 			return False
 
 	def get_images(self):
-		headers = {"User-Agent":"MadeUpBrowser 1.0"}
-		page = r.get(self.url, headers=headers)
+		page = r.get(self.url, headers=self.headers)
 		soup = bs(page.content, "html.parser")
 		img_tags = soup.find_all("img")
 		dl_imgs = []
@@ -35,21 +36,27 @@ class Wallpapercave:
 
 		dl_imgs = set(dl_imgs)
 
-		write_images(dl_imgs, headers)
+		self.write_images(dl_imgs)
 
-	def write_images(self, dl_imgs, headers):
+	def write_images(self, dl_imgs):
+		l = 0
 		current_d = os.getcwd()
 		image_dir = f"{current_d}/images"
-		if not os.isdir(image_dir):
+		if os.path.isdir(image_dir):
+			dirinfo = list(os.walk(image_dir))
+			l = len(dirinfo[0][2])
+
+		else:
 			os.mkdir(image_dir)
+
 		for num, image in enumerate(dl_imgs):
 			imgurl = self.http + self.default + "/" + image
-			dl_img = r.get(imgurl,  headers=headers, stream=True)
-			if dl_img.status_code == 200:
-				with open(f"{image_dir}/image_{num}.jpg", "wb") as f:
-					dl_img.raw.decode_content = True
-					shutil.copyfileobj(dl_img.raw, f)
-
+			if "webp" not in imgurl:
+				dl_img = r.get(imgurl,  headers=self.headers, stream=True)
+				if dl_img.status_code == 200:
+					with open(f"{image_dir}/image_{num+l}.jpg", "wb") as f:
+						dl_img.raw.decode_content = True
+						shutil.copyfileobj(dl_img.raw, f)
 
 
 
